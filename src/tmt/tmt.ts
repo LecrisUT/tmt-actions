31bd9a93091a0b57befad0aea1cda24ea812d373
Cristian Le <git@lecris.dev>
2023-12-29 10:51:39 +0100

    Initial run-tmt action
    Signed-off-by: Cristian Le <git@lecris.dev>

--- src/tmt/tmt.ts
+++ src/tmt/tmt.ts
@@ -3,6 +3,10 @@ import assert from "node:assert"
 
 class Tmt {
 	_version?: string
+	context: { [key: string]: string } = {}
+	constructor(context?: { [key: string]: string }) {
+		if (context !== undefined) this.context = context
+	}
 
 	public async install(version?: string) {
 		// Do actual tmt install
@@ -18,6 +22,43 @@ class Tmt {
 			assert(this._version !== undefined)
 			return this._version
 		})()
+	}
+
+	get context_args(): string[] {
+		let args: string[] = []
+		for (const [key, val] of Object.entries(this.context))
+			args.push("--context", `${key}=${val}`)
+		return args
+	}
+
+	public async plans_ls(root?: string) {
+		let base_args = this.context_args
+		if (root) base_args.push("--root", root)
+		return exec("tmt", [...base_args, "plans", "ls"])
+	}
+
+	public async plans_show(root?: string) {
+		let base_args = this.context_args
+		if (root) base_args.push("--root", root)
+		return exec("tmt", [...base_args, "plans", "show"])
+	}
+
+	public async plans_run(report_xml: string, root?: string, debug?: boolean) {
+		let base_args = this.context_args
+		if (root) base_args.push("--root", root)
+		if (debug) base_args.push("-dd")
+
+		// Must run as provision local
+		const provision_args = ["provision", "--update", "--how", "local"]
+		// Report is not parseable by default. Use JUnit output
+		const report_args = ["report", "--how", "junit", "--file", report_xml]
+		await exec("tmt", [
+			...base_args,
+			"run",
+			"--all",
+			...provision_args,
+			...report_args,
+		])
 	}
 }
 
